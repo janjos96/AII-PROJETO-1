5ac00c2b741f3a77f22840a548b5f8eb932efd95
janjos96 <joaoanjos_96@hotmail.com>
2018-03-03 19:19:14 +0000

    first 8 loop

--- Assets/Scripts/LightDetectorScript.cs
+++ Assets/Scripts/LightDetectorScript.cs
@@ -60,13 +60,6 @@ public class LightDetectorScript : MonoBehaviour {
     // Existem limites e por isso, se a força for menor que 0.1 o sensor despreza essa força e não a passa às rodas assim como se for maio que 0.8 este valor não aumenta
 	public float GetLinearOutput()
 	{
-        /*if (strength <= 0.25f) { strength = 0; }
-        if (strength >= 0.75f) { strength = 0; }
-
-        float output = strength;
-
-        if (output == 0.0f) { output = 0.05f; }
-        if (output >= 0.6f) { output = 0.6f; }*/
 
         if (inverse){
             strengthLinear = -strength + 1;
